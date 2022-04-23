b5e5232ae02b7b2959667d282466ebeaf84f1bd5
jiangzc <jiangzc@ad.unc.edu>
2022-04-23 01:06:31 -0400

    fix guess-flip

--- guess-flip.js
+++ guess-flip.js
@@ -3,7 +3,7 @@ var args = minimist(process.argv.slice(2));
 args['call'];
 import {flipACoin} from './modules/coin.mjs';
 if (args.call != 'heads' && args.call != 'tails') {
-	console.log('Error: invalid input', '\n', 'Usage: node guess-flip --call=[heads|tails]');	
+	console.log('Error: no input.\nUsage: node guess-flip --call=[heads|tails]');	
 } else {
 	console.log(flipACoin(args.call));
 }
