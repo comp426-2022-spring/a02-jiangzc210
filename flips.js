de731d7be98f98c2225af689f8e6f5bc3e83c388
jiangzc <jiangzc@ad.unc.edu>
2022-04-22 23:15:57 -0400

    fix flips.js

--- flips.js
+++ flips.js
@@ -3,4 +3,5 @@ let args = minimist(process.argv.slice(2));
 args['number'];
 let number = args.number || 1;
 import {coinFlips, countFlips} from './modules/coin.mjs';
-console.log(countFlips(coinFlips(number)));
+let flips = coinFlips(number);
+console.log(flips, countFlips(flips));
